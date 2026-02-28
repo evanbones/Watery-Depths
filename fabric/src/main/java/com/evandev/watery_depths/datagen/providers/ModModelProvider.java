803d10aa3773c8ba0ec5978dc94e3063b4b690cf
evanbones <74276616+evanbones@users.noreply.github.com>
2026-02-28 13:38:52 -0800

    Fix item datagen

--- fabric/src/main/java/com/evandev/watery_depths/datagen/providers/ModModelProvider.java
+++ fabric/src/main/java/com/evandev/watery_depths/datagen/providers/ModModelProvider.java
@@ -1,7 +1,9 @@
 package com.evandev.watery_depths.datagen.providers;
 
 import com.evandev.watery_depths.module.ModBlocks;
+import com.evandev.watery_depths.module.ModItems;
 import com.evandev.watery_depths.registration.holders.BlockDataHolder;
+import com.evandev.watery_depths.registration.holders.ItemDataHolder;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.models.BlockModelGenerators;
@@ -10,6 +12,8 @@ import net.minecraft.data.models.model.ModelTemplates;
 import net.minecraft.data.models.model.TextureMapping;
 import net.minecraft.data.models.model.TextureSlot;
 import net.minecraft.resources.ResourceLocation;
+
+import java.util.Map;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -46,5 +50,12 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerators gen) {
+        for (Map.Entry<ResourceLocation, ItemDataHolder<?>> entry : ModItems.getItemRegistry().entrySet()) {
+            ItemDataHolder<?> holder = entry.getValue();
+
+            if (holder.getModel() != null) {
+                gen.generateFlatItem(holder.get(), holder.getModel());
+            }
+        }
     }
 }
